4eea128f201fd683f9398cc1ed57d0ee566414c7
aaron <aaron_lai@hotmail.com>
2018-12-10 20:44:47 +0000

    Created a few more tests and troubleshot price field

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Product, type: :model do
         name: Faker::Name.name,
         description: Faker::Hipster.paragraph,
         quantity: Faker::Number.number(2),
-        price_cents: Faker::Number.number(4)
+        price: Faker::Number.number(4)
       })
     end
 
@@ -20,19 +20,37 @@ RSpec.describe Product, type: :model do
     end
 
     it "is not valid without a name" do
-      expect(@product).to be_valid
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "produces an error when saved without a name" do
+      @product.name = nil
+      @product.save
+      expect(@product.errors).to include(:name)
     end
 
     it "is not valid without a price" do
-      expect(@product).to be_valid
+      @product.price = nil
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "produces an error when saved without a price" do
+      @product.price = nil
+      @product.price_cents = nil
+      @product.save
+      expect(@product.errors).to include(:price || :price_cents)
     end
 
     it "is not valid without a quantity" do
-      expect(@product).to be_valid
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
     end
 
     it "is not valid without a category" do
-      expect(@product).to be_valid
+      @product.category = nil
+      expect(@product).to_not be_valid
     end
 
   end
